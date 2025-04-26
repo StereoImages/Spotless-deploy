335c5b143e59100b95606ef280293eedec70d259
Evan <elburgett@gmail.com>
2025-04-25 21:50:00 -0600

    fix: add use client directive for Vercel deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b2bda6edcfa25549"; }
+/******/ 		__webpack_require__.h = function() { return "b76fcc4933dc9691"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
